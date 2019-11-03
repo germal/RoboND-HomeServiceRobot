2ea134705a1f488fa2d4a7f64ecbf175f5211b6a
Valerio Magnago <valerio.magnago@hotmail.it>
2019-11-03 11:36:13 +0100

    fix: Initial pose of amcl node fixed

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -3,7 +3,7 @@ xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/v
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 10
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/valerio/workspace/HomeService/src/map/map.yaml initial_pose_a:=-1.57" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/valerio/workspace/HomeService/src/map/map.yaml initial_pose_a:=0" &
 sleep 1
 xterm -e " rosrun add_markers add_markers_node" &
 sleep 1
